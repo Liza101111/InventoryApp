8f04486ca8eaa9c6cf1fdb296d726779eefdd47c
Liza <lixia101111@gmail.com>
2023-03-13 10:58:09 +0200

    Add app controller and homepage

---
+++ src/main/java/com/springboot/inventoryapp/controller/AppController.java
@@ -0,0 +1,14 @@
+package com.springboot.inventoryapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+
+}
