32a4a6bf410eb59ace79cf1d0158b71e1693ea37
Liza <lixia101111@gmail.com>
2023-03-28 13:31:49 +0300

    Create Category class and CategoryRepository Interface

---
+++ src/main/java/com/springboot/inventoryapp/entity/Category.java
@@ -0,0 +1,20 @@
+package com.springboot.inventoryapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 45, nullable = false, unique = true)
+    private String name;
+}
---
+++ src/main/java/com/springboot/inventoryapp/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.inventoryapp.repository;
+
+import com.springboot.inventoryapp.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
