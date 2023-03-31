d45805caf1df7f6a98ce57382eaf916c07ba060c
Liza <lixia101111@gmail.com>
2023-03-31 15:58:47 +0300

    Add Product entity and repository

---
+++ src/main/java/com/springboot/inventoryapp/entity/Product.java
@@ -0,0 +1,26 @@
+package com.springboot.inventoryapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 128, nullable = false, unique = true)
+    private String name;
+
+    private float price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
---
+++ src/main/java/com/springboot/inventoryapp/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.inventoryapp.repository;
+
+import com.springboot.inventoryapp.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
