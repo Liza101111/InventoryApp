3abb8ee04852455f36ff5af46aa7cd2411f71517
Liza <lixia101111@gmail.com>
2023-03-31 11:40:08 +0300

    Add save category method

--- src/main/java/com/springboot/inventoryapp/controller/CategoryController.java
+++ src/main/java/com/springboot/inventoryapp/controller/CategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -26,6 +27,12 @@ public class CategoryController {
     public String createNewCategory(Model model){
         model.addAttribute("category", new Category());
         return "create_category";
+    }
+
+    @PostMapping("/categories/save")
+    public String saveCategory(Category category){
+        categoryRepository.save(category);
+        return"redirect:/categories";
     }
 
 }
