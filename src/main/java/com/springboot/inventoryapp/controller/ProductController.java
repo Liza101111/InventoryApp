0bf536f698ff25a52b0cb43a47a75b3250058d01
Liza <lixia101111@gmail.com>
2023-04-01 15:20:23 +0300

    Add category list onto product Model, so we can select the product category

--- src/main/java/com/springboot/inventoryapp/controller/ProductController.java
+++ src/main/java/com/springboot/inventoryapp/controller/ProductController.java
@@ -1,11 +1,15 @@
 package com.springboot.inventoryapp.controller;
 
+import com.springboot.inventoryapp.entity.Category;
 import com.springboot.inventoryapp.entity.Product;
+import com.springboot.inventoryapp.repository.CategoryRepository;
 import com.springboot.inventoryapp.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -13,9 +17,14 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @GetMapping("/products/new")
     public String createProduct(Model model){
+        List<Category> categoryList = categoryRepository.findAll();
         model.addAttribute("product", new Product());
+        model.addAttribute("listCategories", categoryList);
         return "create_product";
     }
 
