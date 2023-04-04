e6d0a9634e11fd7fc0d2231413e42af2a0aee0f8
Liza <lixia101111@gmail.com>
2023-04-04 15:54:56 +0300

    Add two classes and ManyToMany relationship

---
+++ src/main/java/com/springboot/inventoryapp/entity/Role.java
@@ -0,0 +1,17 @@
+package com.springboot.inventoryapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 45, nullable = false, unique = true)
+    private String name;
+
+}
---
+++ src/main/java/com/springboot/inventoryapp/entity/User.java
@@ -0,0 +1,29 @@
+package com.springboot.inventoryapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 45, nullable = false, unique = true)
+    private String email;
+    @Column(length = 25, nullable = false)
+    private String password;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+}
