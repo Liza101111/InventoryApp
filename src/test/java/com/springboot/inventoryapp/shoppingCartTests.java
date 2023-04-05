a198f584aaa21ba057c79298e6ab7a1b4a133629
Liza <lixia101111@gmail.com>
2023-04-05 12:45:07 +0300

    Test add new Item

---
+++ src/test/java/com/springboot/inventoryapp/shoppingCartTests.java
@@ -0,0 +1,35 @@
+package com.springboot.inventoryapp;
+
+import com.springboot.inventoryapp.entity.CartItem;
+import com.springboot.inventoryapp.entity.Product;
+import com.springboot.inventoryapp.entity.User;
+import com.springboot.inventoryapp.repository.CartItemRepository;
+import com.springboot.inventoryapp.repository.CategoryRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class shoppingCartTests {
+
+    @Autowired
+    private CartItemRepository cartItemRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testAddItem(){
+        Product product= entityManager.find(Product.class, 4);
+        User user = entityManager.find(User.class, 6);
+
+        CartItem cartItem = new CartItem(1, product, user);
+        cartItemRepository.save(cartItem);
+    }
+
+}
